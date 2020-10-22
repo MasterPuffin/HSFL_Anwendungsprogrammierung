43446b750a803cd9d43f7d69a9c0fd49a6297d04
Johannes <johannes.bluhm@outlook.com>
2020-10-22 13:49:51 +0200

    changed default namespaces

--- ClientApp013/Ui.Desktop/MainWindow.xaml.cs
+++ ClientApp013/Ui.Desktop/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Ui.Desktop {
+namespace De.HsFlensburg.ClientApp013.Ui.Desktop {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
