3d3564293b5a83924516004acde06443d5635c52
Johannes <johannes.bluhm@outlook.com>
2020-11-08 16:30:10 +0100

    add new model

---
+++ ClientApp013/Logic.Ui/Wrapper/ClientCollectionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using De.HsFlensburg.ClientApp013.Business.Model.BusinessObjects;
+
+namespace De.HsFlensburg.ClientApp013.Logic.Ui.Wrapper {
+    public class ClientCollectionViewModel {
+        private Client myClient = new Client();
+
+        public int Id {
+            get {
+                return myClient.Id;
+            }
+            set {
+                myClient.Id = value;
+            }
+        }
+
+        public string Name {
+            get {
+                return myClient.Name;
+            }
+            set {
+                myClient.Name = value;
+            }
+        }
+    }
+}
---
+++ ClientApp013/Logic.Ui/Wrapper/ClientViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp013.Logic.Ui.Wrapper {
+    public class ClientViewModel {
+        private int id;
+        public int Id {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        private string name;
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}
