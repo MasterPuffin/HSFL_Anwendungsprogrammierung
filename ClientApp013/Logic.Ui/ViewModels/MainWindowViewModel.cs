435eb5e06fd80c16fed0bd06103d35c01ffc8719
Johannes <johannes.bluhm@outlook.com>
2020-11-22 15:39:12 +0100

    Abgabe 5

---
+++ ClientApp013/Logic.Ui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using De.HsFlensburg.ClientApp013.Logic.Ui.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp013.Logic.Ui.ViewModels {
+    public class MainWindowViewModel {
+        public ClientCollectionViewModel MyList { get; set; }
+        public RelayCommand AddClientToList { get; }
+
+        public MainWindowViewModel() {
+            AddClientToList = new RelayCommand(() => AddClientToListMethod());
+            MyList = new ClientCollectionViewModel();
+        }
+
+        private void AddClientToListMethod() {
+            ClientCollectionViewModel list = MyList;
+            if (list != null) {
+                ClientViewModel clientVM = new ClientViewModel();
+                //clientVM.Id = Int16.Parse(IdTextBox.Text);
+                //clientVM.Name = NameTextBox.Text;
+                list.Add(clientVM);
+            }
+        }
+    }
+}
